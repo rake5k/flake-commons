4aac90190368ef44d152ff5ba5e8dea96f92b72f
Christian Harke <christian@harke.ch>
2022-12-02 16:55:04 +0100

    Update flake inputs and remove flake-utils input

--- flake.nix
+++ flake.nix
@@ -2,14 +2,20 @@
   description = "Common flake library";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
-    flake-utils.url = "github:numtide/flake-utils";
-    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
+  outputs = { self, nixpkgs, pre-commit-hooks }:
     let
       name = "flake-commons";
+
       overlay = final: prev: {
         ${name} = prev.${name}.overrideAttrs (old: {
           src = builtins.path {
@@ -18,25 +24,50 @@
           };
         });
       };
+
+      # System types to support.
+      supportedSystems = [ "aarch64-linux" "x86_64-linux" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = [ overlay ];
+      });
     in
-    flake-utils.lib.eachSystem
-      [ "aarch64-linux" "x86_64-linux" ]
-      (system:
+    {
+
+      lib = import ./lib;
+
+      checks = forAllSystems (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
         in
-        rec {
-
-          lib = import ./lib;
-
-          checks = {
-            pre-commit-check = pre-commit-hooks.lib.${system}.run {
-              src = ./.;
-              hooks.nixpkgs-fmt.enable = true;
+        {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              nixpkgs-fmt.enable = true;
+              shellcheck.enable = true;
             };
           };
 
-          devShell = pkgs.mkShell {
+          shellcheck = pkgs.runCommand "shellcheck" { } ''
+            shopt -s globstar
+            echo 'Running shellcheck...'
+            ${pkgs.shellcheck}/bin/shellcheck --check-sourced --enable all --external-sources --shell bash ${./.}/**/*.sh
+            touch ${placeholder "out"}
+          '';
+        });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
             inherit name;
 
             buildInputs = with pkgs; [
@@ -45,13 +76,14 @@
               lolcat
 
               nixpkgs-fmt
+              shellcheck
             ];
 
             shellHook = ''
               figlet ${name} | lolcat --freq 0.5
-              ${(checks.pre-commit-check).shellHook}
+              ${(self.checks.${system}.pre-commit-check).shellHook}
             '';
           };
-        }
-      );
+        });
+    };
 }
