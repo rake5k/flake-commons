c48f823341572584fffc8d2cdb1a21686b0a613e
Christian Harke <christian@harke.ch>
2026-09-22 22:36:13 +0200

    chore: fetch nixpkgs from the channel tarball
    Replace the github:nixos/nixpkgs/nixos-unstable input with
    https://channels.nixos.org/nixos-unstable/nixexprs.tar.zst.
    
    Resolution no longer goes through the GitHub API. The channel tarball
    carries .git-revision, so rev and shortRev remain available on the input.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Common flake library";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.zst";
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
